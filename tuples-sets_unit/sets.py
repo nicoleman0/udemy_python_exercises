85e0924c175bd5e87659b9505f9e0fdedb577f8e
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-07 14:12:13 -0500

    Add examples for set methods including add, remove, copy, clear, and union operations in sets.py

--- tuples-sets_unit/sets.py
+++ tuples-sets_unit/sets.py
@@ -8,4 +8,26 @@ set({1, 2, 3}) # using set function
 for number in s:
     print(number)
 
-# set methods
+# add method (doesn't add if value already in)
+s = set([1, 2, 3, 4])
+s.add(5)
+print(s) # Output 1, 2, 3, 4, 5
+
+# remove
+s1 = {1, 2, 3, 4}
+s1.remove(2)
+print(s1) = {1, 3, 4}
+s1.discard(3) # this is the same idea
+
+# copy
+anotherS = s1.copy()
+
+#clear 
+anotherS.clear()
+
+# union example (set math)
+math_students = ["Nick", "Henry", "Melissa", "Hugo", "Bob"]
+biology_students = ["Henry", "Mark", "Sofia", "Bob"]
+
+math_students | biology_students # or
+math_students & biology_students # and
