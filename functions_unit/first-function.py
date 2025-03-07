4ed958b356c5680e32946cdda10f725da8607dfe
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-07 15:40:43 -0500

    Add a function to print a birthday song in first-function.py

---
+++ functions_unit/first-function.py
@@ -0,0 +1,7 @@
+def happy_bday():
+    print("Happy birthday to you!")
+    print("Happy birthday to you!")
+    print("Happy birthday, dear You!")
+    print("Happy birthday to you!")
+
+happy_bday()
