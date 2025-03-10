083309cb6bf50c9eea626f3ed78fb2821e8eef23
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-10 13:53:19 -0400

    add math function to demonstrate usage of default parameters with add function

--- functions_unit/values.py
+++ functions_unit/values.py
@@ -9,3 +9,10 @@ def add(a=10, b=20): # a and b are optional and default to 10 and 20
     return a + b
 
 print(add()) # 30
+
+# Example: Using default parameters with a function
+def math(a, b, fn=add): # fn is optional and defaults to the add function
+    return fn(a, b)
+
+print(math(10, 20)) # 30
+print(math(30, 20, exponent)) # 900
