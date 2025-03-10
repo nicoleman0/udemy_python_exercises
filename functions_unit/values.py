8352a076c011781f672a445e45b9e654d321ed77
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-10 13:46:12 -0400

    add add function with default parameters and update exponent function

--- functions_unit/values.py
+++ functions_unit/values.py
@@ -1,4 +1,11 @@
+# Default parameters
+
 def exponent(num, power=2): # power is optional and defaults to 2
     return num ** power
 
 print(exponent(9))  # 81
+
+def add(a=10, b=20): # a and b are optional and default to 10 and 20
+    return a + b
+
+print(add()) # 30
