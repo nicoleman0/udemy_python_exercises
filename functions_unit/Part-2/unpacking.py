a60cf0bef1e3411790239d9e60fb25046644f35b
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-11 14:24:15 -0400

    enhance unpacking.py with examples of unpacking lists and dictionaries using * and ** operators

--- functions_unit/Part-2/unpacking.py
+++ functions_unit/Part-2/unpacking.py
@@ -1,4 +1,4 @@
-# using * as an argument
+# unpacking lists and tuples with * operator
 def sum_all_values(*args):
    total = 0
    for num in args:
@@ -7,3 +7,18 @@ def sum_all_values(*args):
 
 nums = [1, 2, 3, 4, 5, 6]
 sum_all_values(*nums) # * works as an unpacking operator with lists and tuples
+
+# unpacking dictionaries with ** operator
+def display_users(user1, user2):
+   return f"{user1} and {user2} are users."
+
+users = {"user1": "Alice", "user2": "Bob"}
+display_users(**users) # ** works as an unpacking operator with dictionaries
+
+# another example of unpacking dictionaries with ** operator
+def add_and_multiply_numbers(a, b, c):
+   return a + b * c
+
+data = dict(a=1000, b=2000, c=3000)
+add_and_multiply_numbers(**data) # ** works as an unpacking operator with dictionaries
+add_and_multiply_numbers(**data, d=4000) # you can add more argyments
