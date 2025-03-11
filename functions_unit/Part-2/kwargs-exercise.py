b7ec74d56098678ea05e9fe5ea2353dfd0cadc82
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-11 14:34:41 -0400

    add kwargs-exercise.py with calculate function for arithmetic operations using keyword arguments

---
+++ functions_unit/Part-2/kwargs-exercise.py
@@ -0,0 +1,25 @@
+# Assignment: Write a function called calculate that accepts a list of keyword arguments
+
+def calculate(**kwargs):
+    make_float = kwargs.get('make_float', False)
+    operation = kwargs.get('operation')
+    first = kwargs.get('first')
+    second = kwargs.get('second')
+    message = kwargs.get('message', None)
+    
+    if operation == 'add':
+        result = first + second
+    elif operation == 'subtract':
+        result = first - second
+    elif operation == 'multiply':
+        result = first * second
+    elif operation == 'divide':
+        result = first / second
+    
+    if make_float:
+        result = float(result)
+
+    if message:
+        return message + ' ' + str(result)
+    else:
+        return 'The result is ' + str(result)
