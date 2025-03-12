eab819449b6b485bd2e829769fd04df7e5aa675d
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-12 12:41:07 -0400

    add map.py to demonstrate the usage of the map function with a lambda for doubling values

---
+++ Lambdas/map.py
@@ -0,0 +1,4 @@
+# Map function is used to apply a function to all the elements of specified iterable and return map object.
+# Syntax: map(function, iterable)
+nums = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+doubles = list(map(lambda x: x * 2, nums))
