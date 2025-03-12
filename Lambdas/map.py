92080a047f25e847d147d08f2e6a1adfe7e8e604
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-12 12:54:04 -0400

    enhance map.py with additional examples using map function for string manipulation and list processing

--- Lambdas/map.py
+++ Lambdas/map.py
@@ -2,3 +2,24 @@
 # Syntax: map(function, iterable)
 nums = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 doubles = list(map(lambda x: x * 2, nums))
+
+people = ['John', 'Mike', 'Sarah', 'Sally']
+upper = list(map(lambda name: name.upper(), people))
+
+if __name__ == '__main__':
+    print(doubles)
+    print(upper)
+
+# Example 2 - Extracting first names from a list of dictionaries
+
+names = [{'first': 'John', 'last': 'Doe'}, {'first': 'Mike', 'last': 'Smith'}, {'first': 'Sarah', 'last': 'Connor'}]
+
+first_names = list(map(lambda x: x['first'], names))
+
+if __name__ == '__main__':
+    print(first_names)
+
+# Example 3 - Decrement all the elements of a list by 1
+
+def decrement_list(num_list):
+    return list(map(lambda x: x-1, num_list))
