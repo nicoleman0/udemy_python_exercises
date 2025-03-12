49583634144a2cd8cd400901d3cc8047480b7f6c
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-12 15:11:05 -0400

    refactor any_all.py to use generator expressions for improved performance

--- built-in_functions/any_all.py
+++ built-in_functions/any_all.py
@@ -9,14 +9,14 @@ all([num for num in [4, 2, 10, 6, 8] if num % 2 == 0]) # True - all numbers are 
 # all - practical example
 
 people = ['Nick', 'Jeremy', 'Charles', 'Amy', 'Arnold']
-all([name[0] == 'A' for name in people]) # False - not all names start with 'A'
+all(name[0] == 'A' for name in people) # False - not all names start with 'A'
 
 nums = [2, 4, 6, 8, 10]
-all([num % 2 == 0 for num in nums])
+all(num % 2 == 0 for num in nums)
 
 # any - returns True if any element of the iterable is true. If the iterable is empty, returns False
 
-any([val for val in [1, 2, 3, 4] if val > 2]) # True - at least one value is greater than 2
+any(val for val in [1, 2, 3, 4] if val > 2) # True - at least one value is greater than 2
 
 nums2 = [1, 3, 4, 5, 7, 10, 19, 27]
-any([num for num in nums2 if num % 2 != 0]) # True - at least one number is odd
+any(num for num in nums2 if num % 2 != 0) # True - at least one number is odd
