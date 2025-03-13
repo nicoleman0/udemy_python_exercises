7ff23c8e35150845b0fb086a616610f982681749
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-13 14:24:47 -0400

    aa

---
+++ built-in_functions/len.py
@@ -0,0 +1,17 @@
+# Description: how len works behind the scenes
+
+len('hi')
+
+'hi'.__len__() # this is how len works behind the scenes
+
+# example of using .__len__()
+
+class SpecialList:
+    def __init__(self, data):
+        self.__data = data
+
+    def __len__(self): # just look here
+        return 50 # returns 50 for length no matter what
+    
+l1 = SpecialList([1, 2, 3])
+print(len(l1)) # 50
