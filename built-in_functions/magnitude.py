80d92b164feccf83116b3fb8691858b193d80b31
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-13 15:47:26 -0400

    add sum_floats function to calculate the sum of float arguments and create zip utility examples

--- built-in_functions/magnitude.py
+++ built-in_functions/magnitude.py
@@ -6,3 +6,7 @@ print(max_magnitude([300, 20, -900]))  # 900
 # define sum_even_values
 def sum_even_values(*args):
     return sum(arg for arg in args if arg % 2 == 0)
+
+# define sum_floats
+def sum_floats(*args):
+    return sum(num for num in args if type(num) == float)
---
+++ built-in_functions/zip.py
@@ -0,0 +1,26 @@
+# zip - combine two or more iterables into a single iterable
+# zip stops when the shortest input iterable is exhausted (zip is lazy)
+first_zip = zip([1, 2, 3], [4, 5, 6])
+list(first_zip) # [(1, 4), (2, 5), (3, 6)]
+dict(first_zip) # {1: 4, 2: 5, 3: 6}
+
+# example - combine two lists into a list of tuples
+nums1 = [1, 2, 3, 4, 5]
+nums2 = [6, 7, 8, 9, 10]
+
+z = zip(nums1, nums2) # <zip object at 0x7f9b1c5f3d40>
+list(z) # [(1, 6), (2, 7), (3, 8), (4, 9), (5, 10)]
+
+# example of zip stopping when the shortest input iterable is exhausted
+nums1 = [1, 2, 3]
+nums2 = [4, 5, 6, 7, 8]
+
+z = zip(nums1, nums2)
+list(z) # [(1, 4), (2, 5), (3, 6)]
+
+# example - combine three lists into a list of tuples
+nums1 = [1, 2, 3]
+nums2 = [4, 5, 6]
+words = ['one', 'two', 'three']
+z2 = zip(nums1, nums2, words)
+list(z2) # [(1, 4, 'one'), (2, 5, 'two'), (3, 6, 'three')]
