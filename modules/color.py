583ab99dbeecb79594ad070314b88b10f09aae0d
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-15 18:07:41 -0400

    update color.py to enhance test script with additional formatting options

--- modules/color.py
+++ modules/color.py
@@ -1,6 +1,7 @@
-import termcolor
+from termcolor import colored
 
-test_script = termcolor.colored(text="This is officially a test script.", color="cyan")
+test_script = colored(text="This is officially a test script.", color="cyan", on_color="on_magenta", attrs=["blink"])
 print(test_script)
 
-
+name = colored(text="Nicholas", on_color="on_red", attrs= ["blink"])
+print(name)
