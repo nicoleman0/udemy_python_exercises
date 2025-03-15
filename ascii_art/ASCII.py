dfb96322a1f8ebfd819061867edfe16cd4f72862
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-15 18:39:06 -0400

    refactor ASCII.py to encapsulate ASCII art printing in a dedicated function with color validation

--- ascii_art/ASCII.py
+++ ascii_art/ASCII.py
@@ -1,10 +1,17 @@
 from pyfiglet import figlet_format
 from termcolor import colored
 
+def print_artwork(msg, color):
+    legit_colors = ('black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'light_grey', 'dark_grey', 'light_red', 
+ 'light_green', 'light_yellow', 'light_blue', 'light_magenta', 'light_cyan')
+    if color.lower() not in legit_colors:
+        color = 'blue'
+
+    ascii_art = figlet_format(msg)
+    colored_ascii = colored(ascii_art, color=color.lower())
+    print(colored_ascii)
+
 msg = input("What would you like to print out?\n")
 color = input("What color would you like to choose?\n")
 
-ascii_art = figlet_format(msg)
-colored_ascii = colored(ascii_art, color=color.lower())
-
-print(colored_ascii)
+print_artwork(msg, color)
