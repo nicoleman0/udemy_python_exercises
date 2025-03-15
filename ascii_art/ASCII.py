aba2229b3e73e8bcb48507516f0f541de0a59a93
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-15 18:45:48 -0400

    updates

--- ascii_art/ASCII.py
+++ ascii_art/ASCII.py
@@ -2,6 +2,10 @@ from pyfiglet import figlet_format
 from termcolor import colored
 
 def print_artwork(msg, color, font):
+    """
+    Prints out a colorized ASCII styled message with the font of your choice.
+    """
+    
     legit_colors = ('black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'light_grey', 'dark_grey', 'light_red', 
  'light_green', 'light_yellow', 'light_blue', 'light_magenta', 'light_cyan')
     
