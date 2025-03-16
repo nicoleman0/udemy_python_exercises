567c63e4e7ad8238383a8f65b853d3c304e5aef9
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-16 12:49:25 -0400

    refactor print_artwork function to improve color and font validation

--- ascii_art/ASCII.py
+++ ascii_art/ASCII.py
@@ -1,16 +1,17 @@
 from pyfiglet import figlet_format
 from termcolor import colored
 
+
 def print_artwork(msg, color, font):
     """
     Prints out a colorized ASCII styled message with the font of your choice.
     """
-    
-    legit_colors = ('black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'light_grey', 'dark_grey', 'light_red', 
- 'light_green', 'light_yellow', 'light_blue', 'light_magenta', 'light_cyan')
-    
+
+    legit_colors = ('black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'light_grey', 'dark_grey', 'light_red',
+                    'light_green', 'light_yellow', 'light_blue', 'light_magenta', 'light_cyan')
+
     legit_fonts = ('doom', 'standard', 'slant')
-    
+
     if color.lower() not in legit_colors:
         color = 'blue'
     if font not in legit_fonts:
@@ -19,6 +20,7 @@ def print_artwork(msg, color, font):
     ascii_art = figlet_format(msg, font=font.lower())
     colored_ascii = colored(ascii_art, color=color.lower())
     print(colored_ascii)
+
 
 msg = input("What would you like to print out?\n")
 color = input("What color would you like to choose?\n")
