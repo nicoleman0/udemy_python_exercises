241f56a8ae61d78349e236bd0792916f3951c3d7
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-17 14:08:43 -0400

    add User and Vehicle classes with initialization and attribute access examples

--- OOP/user-example.py
+++ OOP/user-example.py
@@ -1,5 +1,23 @@
 class User:
-    pass
+    def __init__(self, first, last, age):  # self is a way of referring to specific instances
+        self.first = first  # the varibale name doesn't have to match the parameter name
+        self.last = last
+        self.age = age
 
 
-user1 = User()
+user1 = User("Nick", "Coleman", 24)
+user2 = User("Bob", "Builder", 50)
+
+print(user1.first)   # to access specific attributes you run instance.attribute_name
+print(user2.first, user2.last, user2.age)
+
+
+class Vehicle:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+
+toyota = Vehicle("Toyota", "Camry", 2024)
+print(toyota.make, toyota.model, toyota.year)
