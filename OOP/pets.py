4eff2dab8142aa39ae786c4c5ec4c269fda7ff92
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-18 14:04:39 -0400

    fix Pet class species validation and add set_species method

--- OOP/pets.py
+++ OOP/pets.py
@@ -1,12 +1,20 @@
 class Pet:
+
     allowed = ["cat", "dog", "fish", "rat", "bird", "hamster"]
 
     def __init__(self, name, species):
-        if self.species not in Pet.allowed:
+        if species not in Pet.allowed:
             raise ValueError(f"You can't have a {self.species} pet!")
         self.name = name
         self.species = species
 
+    def set_species(self, species):  # to prevent changing species
+        if species not in Pet.species:
+            raise ValueError(f"You can't have a {self.species} pet!")
+        self.species = species
+
 
 cat = Pet("Franky", "cat")
-dog = Pet("Fido", "dog")
+dog = Pet("Lorenzo", "dog")
+
+print(f"{dog.name} the {dog.species}")
