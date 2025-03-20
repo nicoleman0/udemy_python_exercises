f4d5b22e1aa6b02cba2fe3f4148a2b29b829ddbd
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-20 13:39:54 -0400

    add Character and NPC classes; remove commented age methods from Human class

--- OOP_2/inheritance.py
+++ OOP_2/inheritance.py
@@ -10,15 +10,6 @@ class Human:
         else:
             self._age = 0
 
-    # def get_age(self):
-    #     return self._age
-
-    # def set_age(self, new_age):
-    #     if new_age >= 0:
-    #         self._age = new_age
-    #     else:
-    #         self._age = 0
-
     @property
     def age(self):
         return self._age
