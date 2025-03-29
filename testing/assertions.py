fd29c19f3803e4bd2d6666cfd00eb9447fc4c8ed
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-28 21:07:03 -0400

    add assertions for positive numbers and junk food validation

--- testing/assertions.py
+++ testing/assertions.py
@@ -1,7 +1,18 @@
 def add_positive_numbers(x, y):
+    # if not valid_expression: raise AssertionError()
     assert x > 0 and y > 0, "Both numbers MUST be positive."
     return x + y
 
 
 add_positive_numbers(1, 2)  # 3
-add_positive_numbers(-1, 2)  # AssertionError: Both numbers MUST be positive.
+# add_positive_numbers(-1, 2)  # AssertionError: Both numbers MUST be positive.
+
+
+def eat_junk(food):
+    assert food in ["burger", "chocolate bar",
+                    "hot dog", "fried chicken", "ice cream"], "Food must be junk food!"
+    return f"Mmmm... That {food} is so delicious."
+
+
+burger = eat_junk("burger")
+print(burger)
