832d1460a3dff95a87dac893471605a0b9a1cd88
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-30 15:30:14 -0400

    add functionality to read, parse, and write CSV files

--- CSV_and_Pickling/read.py
+++ CSV_and_Pickling/read.py
@@ -1,28 +1,29 @@
 # THIS DOES READ THE FILE BUT IT DOESN'T PARSE IT!
 # BAD!!!!!! DON'T DO THIS!
-with open("fighters.csv") as file:
+with open("/home/nicholas-coleman/Desktop/udemy_python/CSV_and_Pickling/fighters.csv") as file:
     data = file.read()
 
 # Using reader
 from csv import reader
-with open("fighters.csv") as file:
-    csv_reader = reader(file)
+with open("/home/nicholas-coleman/Desktop/udemy_python/CSV_and_Pickling/fighters.csv") as file:
+    csv_reader = reader(file, delimiter=",")    # can change delimiter
     next(csv_reader)  # To skip the headers
     for fighter in csv_reader:
         # Each row is a list
-        # Use index to access data
-        print(f"{fighter[0]} is from {fighter[1]}")
+        # Use index to access data with reader
+        print(
+            f"{fighter[0]} is from {fighter[1]} and is {fighter[2]} cm tall.")
 
 # Example where data is cast into a list
 from csv import reader
-with open("fighters.csv") as file:
+with open("/home/nicholas-coleman/Desktop/udemy_python/CSV_and_Pickling/fighters.csv") as file:
     csv_reader = reader(file)
     data = list(csv_reader)
     print(data)
 
 # Reading/Parsing CSV Using a DictReader:
 from csv import DictReader
-with open("fighters.csv") as file:
+with open("/home/nicholas-coleman/Desktop/udemy_python/CSV_and_Pickling/fighters.csv") as file:
     csv_reader = DictReader(file)
     for row in csv_reader:
         # Each row is an OrderedDict!
